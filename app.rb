34c6d2b7bd42715f548efd6b1e7bce05bf0d2861
Chris Allick <chrisallick@gmail.com>
2012-09-01 15:57:02 -0700

    needed to add redistogo

--- app.rb
+++ app.rb
@@ -1,17 +1,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'erb'
+require 'redis'
 
 configure do
-  require 'redis'
-  redisUri = ENV["REDISTOGO_URL"]
+  redisUri = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
   uri = URI.parse(redisUri) 
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
 
 get '/' do
 	REDIS.set("dope", "nachos")
-  erb :index, :locals => {
-  	:cool => "dope"
-  }
+  	erb :index, :locals => {
+  		:cool => REDIS.get("dope")
+  	}
 end
